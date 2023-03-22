d63831c71b3cc96d8d9f6fe44048728d99e709d5
yurim3 <w2103@e-mirim.hs.kr>
2023-03-22 09:47:50 +0900

    null과 undefined 비교 코드 추가

--- 동등비교.js
+++ 동등비교.js
@@ -9,3 +9,6 @@ console.log('hello' === 'hello');   //true
 
 console.log(0 == false);    //true
 console.log(0 === false);   //false
+
+console.log(null == undefined);     //true
+console.log(null === undefined);    //false
