3d2a67728b2d581ba3bb8c8b501fecf4a9366b9d
yurim3 <w2103@e-mirim.hs.kr>
2023-03-22 09:06:18 +0900

    ==, === 차이 비교

---
+++ 동등비교.js
@@ -0,0 +1,11 @@
+//동등연산자(abstract eauility) 값만 비교
+console.log(1 == 1);
+
+//일치연산자(strict equility) 자료형까지 비교
+console.log(1 === '1');
+
+console.log('hello' == 'hello'); //true
+console.log('hello' === 'hello');   //true
+
+console.log(0 == false);    //true
+console.log(0 === false);   //false
