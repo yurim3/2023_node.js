d9794b5390366331a8725c9f6afad82446c12fce
yurim3 <w2103@e-mirim.hs.kr>
2023-03-23 13:54:10 +0900

    forEach

---
+++ forEach.js
@@ -0,0 +1,10 @@
+const fruits = ['사과', '바나나', '오렌지'];
+
+for(let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+//forEach문
+fruits.forEach(function(f){
+    console.log(f);
+});
