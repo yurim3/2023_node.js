328ec36dc388da4f17f2316b3138d44ec189b472
yurim3 <w2103@e-mirim.hs.kr>
2023-04-05 09:54:31 +0900

    fs.statSync

--- 문법/fs.js
+++ 문법/fs.js
@@ -5,4 +5,9 @@ console.log(_dirname);
 
 //해당 경로에 있는 파일 목록의 배열을 반환
 const files = fs.readdirSync(__dirname);
-console.log(files);
+
+//특정 파일의 전체(절대) 경로
+result = __dirname + '\\' + files[1];
+console.log(result);
+
+console.log(fs.statSync(result).isFile());
--- 사진관리/main.js
+++ 사진관리/main.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
+//dirPath에 있는 모든 파일목록의 배열(하위경로까지)
 function getAllFiles(dirPath, arrOfFiles) {
+    //dirPath에 있는 모든 파일목록의 배열(하위 경로는 불가)
     const files = fs.readdirSync(dirPath);
+
+    files.forEach(function(file) {
+        if(fs.statSync(dirPath + "\\" + file).isDirectory()) {
+            console.log(dirPath + "\\" + file + "는 디렉토리(폴더)입니다.");
+        }else {
+            console.log(dirPath + "\\" + file + "는 파일입니다.");
+        }
+    });
 }
+
+const files = getAllFiles(__dirname + "\\base", []);
