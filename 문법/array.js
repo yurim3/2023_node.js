f720e03e3a1fe4e7066057cbc7f4e630386d4593
yurim3 <w2103@e-mirim.hs.kr>
2023-04-12 10:17:02 +0900

    array.indexOf()

--- 문법/array.js
+++ 문법/array.js
@@ -9,12 +9,16 @@ arr.forEach(function(data, index) {
 const cherryIndex = arr.findIndex(data => data === 'cherry')
 // console.log(cherryIndex);
 
+//3번 index부터 'cherry'에 해당하는 idx를 찾음, 없으면 -1
+const a = arr.indexOf('cherry', 3);
+console.log(a);
+
 //배열 끝부분에 data 추가
 arr.push('grape');
-console.log(arr);
+// console.log(arr);
 
 //배열 끝부분의 데이터를 꺼냄
 const x = arr.pop();
-console.log(x);
-console.log(arr);
+// console.log(x);
+// console.log(arr);
 
