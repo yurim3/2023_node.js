72292370625ce54cb8a06c81da6494bb3deecd03
yurim3 <w2103@e-mirim.hs.kr>
2023-04-12 10:12:33 +0900

    array.push(), array.pop()

--- 문법/array.js
+++ 문법/array.js
@@ -7,4 +7,14 @@ arr.forEach(function(data, index) {
 
 //return 값이 true에 해당하는 idx 반환, 없으면 -1을 반환
 const cherryIndex = arr.findIndex(data => data === 'cherry')
-console.log(cherryIndex);
+// console.log(cherryIndex);
+
+//배열 끝부분에 data 추가
+arr.push('grape');
+console.log(arr);
+
+//배열 끝부분의 데이터를 꺼냄
+const x = arr.pop();
+console.log(x);
+console.log(arr);
+
