80b64985aa2d39019e4b4fa68ea22e32e2487633
yurim3 <w2103@e-mirim.hs.kr>
2023-04-12 09:49:35 +0900

    path.join()

--- 문법/fs.js
+++ 문법/fs.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 //__dirname: 현재 디렉토리의 절대경로(Node의 전역 변수)
 console.log(_dirname);
@@ -7,7 +8,10 @@ console.log(_dirname);
 const files = fs.readdirSync(__dirname);
 
 //특정 파일의 전체(절대) 경로
-result = __dirname + '\\' + files[1];
+// result = __dirname + '\\' + files[1];
+//문자열 합치는 것보다 join을 사용하는 것을 권장(경로방식을 운영체제에 맞춰서 처리해 줌)
+result = path.join(__dirname, files[1]);
 console.log(result);
 
-console.log(fs.statSync(result).isFile());
+//해당 파일에 대한 정보를 알 수 있음
+console.log(fs.statSync(result));
--- 사진관리/main.js
+++ 사진관리/main.js
@@ -1,23 +1,24 @@
-const { dir } = require('console');
 const fs = require('fs');
 const path = require('path');
 
-//dirPath에 있는 모든 파일목록의 배열(하위경로까지)
+// dirPath에 있는 모든 파일목록의 배열(하위경로까지)
 function getAllFiles(dirPath, arrOfFiles) {
-    //dirPath에 있는 모든 파일목록의 배열(하위 경로는 불가)
-    const files = fs.readdirSync(dirPath);
+  // dirPath에 있는 모든 파일목록의 배열(하위경로는 불가)
+  const files = fs.readdirSync(dirPath);
 
-    files.forEach(function(file) {
-      const destPath = dirPath+"\\"+file;
-      //폴더(디렉토리)라면 해당 폴더에 있는 모든 파일목록들을 체크한다
-        if(fs.statSync(dirPath + "\\" + file).isDirectory()) {
-           getAllFiles(destPath,arrOfFiles);
-        }else {
-          //파일이라면 파일목록에 추가
-            arrOfFiles.push(destPath);
-        }
-    });
-    return arrOfFiles;
+  files.forEach(function(file) {
+    const destPath = path.join(dirPath, file);
+    
+    // 폴더(디렉토리)라면 해당 폴더에 있는 모든 파일목록들을 체크한다
+    if(fs.statSync(destPath).isDirectory()) {
+      getAllFiles(destPath, arrOfFiles);
+    } else {
+    // 파일이라면 파일목록에 추가
+      arrOfFiles.push(destPath);
+    }
+  });
+
+  return arrOfFiles;
 }
 
 const files = getAllFiles(__dirname + "\\base", []);
