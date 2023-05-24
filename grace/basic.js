cd0ae367ebf3ae1588376599435923cd01912d4a
yurim3 <w2103@e-mirim.hs.kr>
2023-05-24 09:08:50 +0900

    파일 스트림을 통한 웹서버 간단하게 구현

--- grace/basic.js
+++ grace/basic.js
@@ -6,16 +6,11 @@ const server = http.createServer(function(req, res) {
   console.log(req.url);
 
   res.setHeader('Content-type', 'text/html');
-  if(req.url === '/') {
+  if(req.url == '/') {
     // 스트리밍 방식으로 index의 html 코드들을 전송 (대용량 처리에 유리)
     // 파일입출력 : 파일을 한꺼번에 처리함
     // 스트림 : 파일을 부분적으로 쪼개서 실시간으로 처리함
     fs.createReadStream(path.join(__dirname, 'html', 'index.html')).pipe(res);
-  }else if(req.url === '/food') {
-    fs.createReadStream(path.join(__dirname, 'html', 'food.html')).pipe(res);
-  }
-  else {
-    fs.createReadStream(path.join(__dirname, 'html', '404.html')).pipe(res);
   }
 });
 
