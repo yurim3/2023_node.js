1b127d358c48a5e3a1c6a20a995bcb3cc83b21fb
yurim3 <w2103@e-mirim.hs.kr>
2023-05-25 14:28:43 +0900

    외우고 다시 쳐보기

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello World");
+app.get('/', function(req, res) {
+    res.send("방가");
 });
 
 app.listen(3333);
