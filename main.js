73409d5f9e736199eaf8609ee05406ee53140de7
yurim3 <w2103@e-mirim.hs.kr>
2023-05-31 09:48:20 +0900

    nodemon 설치 및 익스프레스 '*'

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send("방가");
+// 모든 경로에 요청
+app.get('*', function (req, res, next) {
+    console.log('다들');
+    next();
+})
+
+// 루트(/) 경로에 요청
+app.get('/', function (req, res, next) {
+    console.log('진행시켜');
+    next();
 });
 
 app.listen(3333);
