290f30e1f2b73cdb1d70b5381ce505d92fd71588
yurim3 <w2103@e-mirim.hs.kr>
2023-05-31 10:03:32 +0900

    express static 미들웨어

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'grace', 'html')));
+
 // 모든 경로에 요청
-app.get('*', function (req, res, next) {
-    console.log('다들');
-    next();
+app.get('*', function(req, res, next) {
+  console.log('당장');
+  next();
 })
-
 // 루트(/) 경로에 요청
-app.get('/', function (req, res, next) {
-    console.log('진행시켜');
-    next();
+app.get('/', function(req, res, next) {
+  console.log('진행시켜');
+  next();
 });
-
 // http://localhost:3333/test/JWP?lang=ko
 app.get('/test/:name', function(req, res, next) {
   console.log('path', req.path);      // '/test/JWP'
@@ -20,5 +22,4 @@ app.get('/test/:name', function(req, res, next) {
   console.log('query', req.query);    // {lang: 'ko'}
   res.send('test중 콘솔을 보시오');
 });
-
 app.listen(3333);
