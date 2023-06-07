043b85149c12b0c3c4fd7fd2679008662be3309a
yurim3 <w2103@e-mirim.hs.kr>
2023-06-07 10:18:04 +0900

    .env로 시스템정보 감추기

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
+const path = require('path');
+
+require('dotenv').config({
+    path: path.join(__dirname, '.env')
+});
 
 // mysql 시스템 정보(절대로 공개하면 안됨)
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'lck',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB
 });
 
 pool.query("SELECT * FROM player", (err, results) => {
