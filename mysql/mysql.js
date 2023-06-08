b7f34f1139df677237d428585fef278f73f62f33
yurim3 <w2103@e-mirim.hs.kr>
2023-06-08 14:02:36 +0900

    catch

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,21 +1,25 @@
-const mysql = require('mysql2/promise');
-const path = require('path');
+const mysql = require("mysql2/promise");
+const path = require("path");
 
-require('dotenv').config({
-    path: path.join(__dirname, '.env')
+require("dotenv").config({
+  path: path.join(__dirname, ".env"),
 });
 
 // mysql 시스템 정보(절대로 공개하면 안됨)
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DB
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
 });
 
-pool.query("SELECT * FROM player")
+pool
+  .query("SELECT * FROM player")
   .then(([results]) => {
-    for(const r of results) {
+    for (const r of results) {
       console.log(r);
     }
   })
+  .catch((err) => {
+    console.error(err);
+  });
