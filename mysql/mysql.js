7cfeb3a4c500073d200338a8b6b6429d552206d4
yurim3 <w2103@e-mirim.hs.kr>
2023-06-08 14:12:22 +0900

    INSERT

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -13,6 +13,24 @@ const pool = mysql.createPool({
   database: process.env.DB_DB,
 });
 
+const insertData = {
+  name: "bengi",
+  lane: "jg",
+  team: "t1",
+  kills: 5,
+  deaths: 3,
+  assists: 8,
+};
+
+pool
+  .query("INSERT INTO player SET ?", insertData)
+  .then(() => {
+    console.log("INSERT 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 pool
   .query("SELECT * FROM player")
   .then(([results]) => {
