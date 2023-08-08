4f676556e22575847353d3fb09313d10395cf4c5
yurim3 <w2103@e-mirim.hs.kr>
2023-08-09 08:47:49 +0900

    SELECT 부연설명

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2/promise");
-const path = require("path");
-
-require("dotenv").config({
-  path: path.join(__dirname, ".env"),
+const mysql = require('mysql2/promise');
+const path = require('path');
+require('dotenv').config({
+  path: path.join(__dirname, '.env')
 });
-
 // mysql 시스템 정보(절대로 공개하면 안됨)
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -12,25 +10,14 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DB,
 });
-
 // const insertData = {
-//   name: "bengi",
-//   lane: "jg",
-//   team: "t1",
+//   name: 'bengi',
+//   lane: 'jg',
+//   team: 't1',
 //   kills: 5,
 //   deaths: 3,
-//   assists: 8,
+//   assists: 8
 // };
-
-// pool
-//   .query("INSERT INTO player SET ?", insertData)
-//   .then(() => {
-//     console.log("INSERT 성공");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
 // pool.query("INSERT INTO player SET ?", insertData)
 //   .then(()=>{
 //     console.log('INSERT 성공')
@@ -38,38 +25,33 @@ const pool = mysql.createPool({
 //   .catch(err => {
 //     console.error(err);
 //   });
-
 const updateData = {
   name: "sungwoong",
   kills: 15,
-  deaths: 10,
-};
-
-// pool
-//   .query("UPDATE player SET ? WHERE name = ?", [updateData, "bengi"])
+  deaths: 10
+}
+// pool.query("UPDATE player SET ? WHERE name = ?", [updateData, "bengi"])
 //   .then(() => {
 //     console.log("UPDATE 성공");
 //   })
-//   .catch((err) => {
+//   .catch(err => {
 //     console.error(err);
 //   });
-
-pool
-  .query("DELETE FROM player WHERE name = ?", "sungwoong")
+pool.query("DELETE FROM player WHERE name = ?", "sungwoong")
   .then(() => {
     console.log("DELETE 성공");
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(err);
   });
-  
-pool
-  .query("SELECT * FROM player")
+
+// SELECT문은 결과값을 받아야 한다
+pool.query("SELECT * FROM player")
   .then(([results]) => {
-    for (const r of results) {
+    for(const r of results) {
       console.log(r);
     }
   })
-  .catch((err) => {
+  .catch(err => {
     console.error(err);
   });
