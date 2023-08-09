c582011f43dded30382b8ebe93e51b9b5f9db3b4
yurim3 <w2103@e-mirim.hs.kr>
2023-08-09 10:13:20 +0900

    cookie-parser로 간단하게

--- cookie.js
+++ cookie.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(cookieParser());
+
 app.get("/set", (req, res) => {
-  res.setHeader("Set-Cookie", "name=KY");
-  res.setHeader("Set-Cookie", "age=62");
+  res.cookie("name", "KY");
+  res.cookie("age", 63);
   res.send("쿠키 set");
 });
 
@@ -13,22 +16,11 @@ app.get("/get", (req, res) => {
   //쿠키는 string이다
   const cookie = req.headers.cookie;
   if (cookie) {
-    // "name="이 있는 idx의 시작값을 찾는데, 존재하지 않으면 -1 return
-    const nameStartIdx = cookie.indexOf("name=");
+    const name = req.cookies.name;
 
     // name이 존재한다면
-    if (nameStartIdx > -1) {
-      let name = cookie.substring(nameStartIdx + 5);
-      const nameEndIdx = name.indexOf(";");
-      // 문자열 끝에 ';'이 있으면
-      if (nameEndIdx > -1) {
-        name = name.substring(0, nameEndIdx);
-        res.send(`Hello ${name}`);
-      }
-      // 문자열 끝에 ';'이 없으면(쿠키가 하나만 있거나 name이 마지막 쿠키라면)
-      else {
-        res.send(`Hello ${name}`);
-      }
+    if (name) {
+      res.send(`Hello ${name}`);
     } else {
       res.send("name이 존재하지 않음");
     }
